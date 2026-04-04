8a241cbaa04499cb2c1d29653436f51fa17078ed
glimmeringcandle <1941902161@qq.com>
2026-04-04 22:28:05 +0800

    初始化

---
+++ src/test/java/com/candlesun/online_exam_system/OnlineExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candlesun.online_exam_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
